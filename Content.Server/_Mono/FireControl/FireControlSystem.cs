0d77c255adcbaf0246353b568d62f4db4378a58f
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-06-29 10:03:35 +0000

    chore: automatically update REUSE headers

--- Content.Server/_Mono/FireControl/FireControlSystem.cs
+++ Content.Server/_Mono/FireControl/FireControlSystem.cs
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 RikuTheKiller
+// SPDX-FileCopyrightText: 2025 ScyronX
+// SPDX-FileCopyrightText: 2025 ark1368
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 // Copyright Rane (elijahrane@gmail.com) 2025
 // All rights reserved. Relicensed under AGPL with permission
 
