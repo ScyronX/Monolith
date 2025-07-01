0bc40914a10ed90fe4b14b67348917b365f5e908
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-07-01 10:12:11 +0000

    chore: automatically update REUSE headers

--- Content.Shared/_Shitmed/Surgery/SharedSurgerySystem.Steps.cs
+++ Content.Shared/_Shitmed/Surgery/SharedSurgerySystem.Steps.cs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 ScyronX
+// SPDX-FileCopyrightText: 2025 mikusssssss
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Humanoid;
 using Content.Shared.Humanoid.Markings;
 using Content.Shared.Bed.Sleep;
